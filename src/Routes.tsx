06200922031d42a6587b4318771498ecf54413f5
andres <jorquera.ad@gmail.com>
2023-04-17 16:30:53 +0200

    adding basename

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,19 @@
 import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
 import Settings from './pages/Settings/Settings';
 
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    loader: () => redirect('/settings'),
-  },
-  {
-    path: '/settings/*',
-    element: <Settings />,
-  },
-]);
+const routes = createBrowserRouter(
+  [
+    {
+      path: '/',
+      loader: () => redirect('/settings'),
+    },
+    {
+      path: '/settings/*',
+      element: <Settings />,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 const Routes = () => {
   return <RouterProvider router={routes} />;
